486c508b814edd4121d5fa37b670e5ec30d5d5f8
Zain <zainiscoding@gmail.com>
2020-11-19 22:21:52 +0900

    Added construction text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './styles/reset.css';
 import './styles/main.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div id='App'>
+      <div id='construction-text-container'>
+        <h1>Under construction</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
