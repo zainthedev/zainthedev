67c051cebbe3e8b2ca3e7cba360e56c0b760cf41
Zain Hill <zainthedev@gmail.com>
2021-02-25 11:03:02 +0900

    Changed: Fixed imports for jpg's

--- src/components/ProjectsComponent.jsx
+++ src/components/ProjectsComponent.jsx
@@ -32,7 +32,7 @@ export const ProjectsComponent = ({ forwardedRef }) => {
 							onClick={() => window.open('https://reactagram-zain.web.app/')}
 							alt='Reactagram 1 Wide'
 							srcSet={projectImages.wideReactagram1JPG}
-							src={projectImages.wideReactagram1}
+							// src={projectImages.wideReactagram1}
 						/>
 						<WideProjectImage
 							onClick={() => window.open('https://reactagram-zain.web.app/')}
--- src/projectImageContainer.js
+++ src/projectImageContainer.js
@@ -16,22 +16,22 @@ import wideWeather1 from './images/wideWeather1.webp';
 import wideWeather2 from './images/wideWeather2.webp';
 
 //JPG versions for browsers that don't support webp
-import reactagram1JPG from './images/reactagram1.webp';
-import reactagram2JPG from './images/reactagram2.webp';
-import wideReactagram1JPG from './images/wideReactagram1.webp';
-import wideReactagram2JPG from './images/wideReactagram2.webp';
-import waldo1JPG from './images/waldo1.webp';
-import waldo2JPG from './images/waldo2.webp';
-import wideWaldo1JPG from './images/wideWaldo1.webp';
-import wideWaldo2JPG from './images/wideWaldo2.webp';
-import battleship1JPG from './images/battleship1.webp';
-import battleship2JPG from './images/battleship2.webp';
-import wideBattleship1JPG from './images/wideBattleship1.webp';
-import wideBattleship2JPG from './images/wideBattleship2.webp';
-import weather1JPG from './images/weather1.webp';
-import weather2JPG from './images/weather2.webp';
-import wideWeather1JPG from './images/wideWeather1.webp';
-import wideWeather2JPG from './images/wideWeather2.webp';
+import reactagram1JPG from './images/reactagram1.jpg';
+import reactagram2JPG from './images/reactagram2.jpg';
+import wideReactagram1JPG from './images/wideReactagram1.jpg';
+import wideReactagram2JPG from './images/wideReactagram2.jpg';
+import waldo1JPG from './images/waldo1.jpg';
+import waldo2JPG from './images/waldo2.jpg';
+import wideWaldo1JPG from './images/wideWaldo1.jpg';
+import wideWaldo2JPG from './images/wideWaldo2.jpg';
+import battleship1JPG from './images/battleship1.jpg';
+import battleship2JPG from './images/battleship2.jpg';
+import wideBattleship1JPG from './images/wideBattleship1.jpg';
+import wideBattleship2JPG from './images/wideBattleship2.jpg';
+import weather1JPG from './images/weather1.jpg';
+import weather2JPG from './images/weather2.jpg';
+import wideWeather1JPG from './images/wideWeather1.jpg';
+import wideWeather2JPG from './images/wideWeather2.jpg';
 
 export const projectImages = {
 	reactagram1: reactagram1,
